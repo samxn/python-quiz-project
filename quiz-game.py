360d00d2789c54fb4f72af3db3352abd36009993
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 22:18:29 -0500

    Added question and answer

--- quiz-game.py
+++ quiz-game.py
@@ -1,3 +1,13 @@
 print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
+print(playing)
+
+if playing != "yes":
+    quit()
+
+print("Lets play!")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print('Correct!')
