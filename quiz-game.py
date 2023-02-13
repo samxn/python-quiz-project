76a7987994514923c24dba4459539331c5224ddd
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 22:39:09 -0500

    eliminated case sensitivity

--- quiz-game.py
+++ quiz-game.py
@@ -3,31 +3,31 @@ print("Welcome to my computer quiz!")
 playing = input("Do you want to play? ")
 print(playing)
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Lets play!")
 
 answer = input("The logo for luxury car maker Porsche features which animal? ")
-if answer == "horse":
+if answer.lower() == "horse":
     print('Correct!')
 else:
     print('Incorrect')
 
 answer = input("Which element is said to keep bones strong? ")
-if answer == "calcium":
+if answer.lower() == "calcium":
     print('Correct!')
 else:
     print('Incorrect')
 
 answer = input("What is the first name of the daughter in The Addams Family? ")
-if answer == "wednesday":
+if answer.lower() == "wednesday":
     print('Correct!')
 else:
     print('Incorrect')
 
 answer = input("What does “www” stand for in a website browser? ")
-if answer == "world wide web":
+if answer.lower() == "world wide web":
     print('Correct!')
 else:
     print('Incorrect')
