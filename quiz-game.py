5e371642686147878a3195e121f57248aec6c51d
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 22:34:50 -0500

    Added more questions

--- quiz-game.py
+++ quiz-game.py
@@ -8,6 +8,26 @@ if playing != "yes":
 
 print("Lets play!")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+answer = input("The logo for luxury car maker Porsche features which animal? ")
+if answer == "horse":
     print('Correct!')
+else:
+    print('Incorrect')
+
+answer = input("Which element is said to keep bones strong? ")
+if answer == "calcium":
+    print('Correct!')
+else:
+    print('Incorrect')
+
+answer = input("What is the first name of the daughter in The Addams Family? ")
+if answer == "wednesday":
+    print('Correct!')
+else:
+    print('Incorrect')
+
+answer = input("What does “www” stand for in a website browser? ")
+if answer == "world wide web":
+    print('Correct!')
+else:
+    print('Incorrect')
