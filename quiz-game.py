c90c135a8f1753bb7041379169a4ad69935c4353
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 22:42:33 -0500

    Added score

--- quiz-game.py
+++ quiz-game.py
@@ -7,27 +7,34 @@ if playing.lower() != "yes":
     quit()
 
 print("Lets play!")
+score = 0
 
 answer = input("The logo for luxury car maker Porsche features which animal? ")
 if answer.lower() == "horse":
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
 
 answer = input("Which element is said to keep bones strong? ")
 if answer.lower() == "calcium":
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
 
 answer = input("What is the first name of the daughter in The Addams Family? ")
 if answer.lower() == "wednesday":
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
 
 answer = input("What does “www” stand for in a website browser? ")
 if answer.lower() == "world wide web":
     print('Correct!')
+    score += 1
 else:
     print('Incorrect')
+
+print ("You got " + str(score) + " questions correct!")
